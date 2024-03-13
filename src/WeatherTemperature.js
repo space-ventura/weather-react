fb3d72d7ff0ad9694527d42b1c6907cfaa1684bc
Adele McKenna <theotheradele@gmail.com>
2024-03-12 23:46:07 -0400

    Fixed Unit Conversions

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit , setUnit] = useState("celsius");
@@ -12,7 +12,7 @@ export default function WeatherTemperature(props) {
   setUnit("celsius");
  }
 
-  if (unit === celsius) {
+  if (unit === "celsius") {
   return (
     <div className WeatherTemperature>
       <span className="temperature">{Math.round(props.celsius)}</span>
@@ -24,7 +24,7 @@ export default function WeatherTemperature(props) {
     return (
  <div className WeatherTemperature>
       <span className="temperature">{Math.round(fahrenheit)}</span>
-         <span className="unit"><a href="/" onClick={showFahrenheit}>°C</a> | °F</span>
+         <span className="unit"><a href="/" onClick={showCelsius}>°C</a> | °F</span>
          </div>
          )
   }
