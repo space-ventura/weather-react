171c8d3d6fb15d41743d8e6b5ededb479038cd8d
Adele McKenna <theotheradele@gmail.com>
2024-03-15 18:15:48 -0400

    Added useEffect hook

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,35 +1,38 @@
-import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-let [loaded, setLoaded] = useState(false);
-let [forecast, setForecast] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-function handleResponse(response) {
-setForecast(response.data.daily);
-setLoaded(true);
-}
+  useEffect(()=>{
+    setLoaded(false)
+  }, [props.coordinates])
 
-if (loaded) {
- return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-        <WeatherForecastDay data={forecast[0]}/>
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 
- } else {
-const apiKey = "5ef4de8cd6b7fefcd7c42f98cf464ce8";
-let longitude= props.coordinates.lon;
-let latitude= props.coordinates.lat;
-let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
- axios.get(apiUrl).then(handleResponse);
-  return null;
+  } else {
+    const apiKey = "5ef4de8cd6b7fefcd7c42f98cf464ce8";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import Weather from "./Weather";
 import "./Weather.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
