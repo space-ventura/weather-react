c6e5e5e4ba530f78e7c2de4794ae24b015c904c2
Adele McKenna <theotheradele@gmail.com>
2024-03-17 01:07:56 -0400

    Added daily forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -25,7 +25,7 @@ let days = ["Sun" , "Mon" , "Tue" , "Wed" , "Thu" , "Fri" , "Sat"];
   return (
     <div>
     <div className="WeatherForecast-day">{day()}</div>
-          <WeatherIcon code={props.data.weather[0].icon} size={36}/>
+          <WeatherIcon code={props.data.weather[0].icon} size={36} className="icon"/>
           <div className="WeatherForecast-temperatures">
             <span className="WeatherForecast-temperature-max">{maxTemperature()}</span> 
             <span className="WeatherForecast-temperature-min">{minTemperature()}</span>
